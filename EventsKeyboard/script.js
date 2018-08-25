d135f9f13c6c1914b6e5264384664f4fcb21746a
Breno Nunes <brenontavares@gmail.com>
2018-08-24 23:07:50 -0300

    Eventos de Teclado

--- EventsKeyboard/script.js
+++ EventsKeyboard/script.js
@@ -1,1 +1,12 @@
-function()
+function apertouTecla(event){
+    //console.log("Apertou alguma tecla: "+event.keyCode);
+    if(event.shiftKey == true && event.keyCode == 69) {
+        console.log("Apertou SHIFT + E");
+    }
+}
+/*function UpTecla(event){
+    console.log("Up alguma tecla: "+event.keyCode);
+}
+function PressTecla(event){
+    console.log("Press alguma tecla: "+event.keyCode);
+}*/
