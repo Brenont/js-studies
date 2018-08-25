a34ce886fa1d02f7cd16af541b267784f6f03b5e
Breno Nunes <brenontavares@gmail.com>
2018-08-24 22:10:56 -0300

    Eventos de mouse

---
+++ EventsMouse/script.js
@@ -0,0 +1,27 @@
+/*function ClickMouse(){
+    console.log("Click do Mouse");
+}
+function SoltouClick(){
+    console.log("Soltou o Mouse")
+}
+function OverMouse(){
+    console.log("Over no mouse")
+}
+function MouseSaiuDeCima(){
+    console.log("Mouse saiu de cima")
+}
+function MexerMouse(){
+    console.log("Mouse foi movido")
+}*/
+// ----------------------------
+function Clicou(){
+    console.log("CLICK!")
+}
+function BotaoDireito(){
+    console.log("BOTÃO DIREITO!");
+
+    return false;
+}
+function ClickDuplo(){
+    console.log("CLICOU DUAS VEZES")
+}
