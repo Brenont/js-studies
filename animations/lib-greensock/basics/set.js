327f5ca72871c74e31d69cb92c27fd3e08c14922
Breno Nunes <Brenontavares@gmail.com>
2018-12-19 21:48:42 -0200

    starting greesock studies

---
+++ animations/lib-greensock/basics/set.js
@@ -0,0 +1,14 @@
+// const items = document.querySelectorAll(".item");
+// const title = document.querySelector(".title");
+
+TweenMax.set(items, {
+    x: 100,
+    y: -100,
+    opacity: 0.5,
+    transition: "0.5s",
+})
+
+
+TweenMax.set(title, {
+    color: "#123456",
+})
